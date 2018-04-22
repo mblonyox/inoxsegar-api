b7ffafd6ec62496c41b554a718160834201b27e0
mblonyox <mblonyox@gmail.com>
2018-04-22 13:51:05 +0700

    Tambah validasi dan query untuk pencarian file.

--- routes/file.js
+++ routes/file.js
@@ -1,14 +1,34 @@
 const express = require('express')
+const { query } = require('express-validator/check')
 
 const File = require('../models/file')
 const verifyToken = require('../middlewares/verify_token')
+const checkValidation = require('../middlewares/check_validation')
+const { catchErr } = require('../helpers')
 
 const router = express.Router();
 
 router.use(verifyToken);
 
-router.get('/file', (req, res) => {
-  File.find({}, null, {limit: 20, sort: '-date'})
+router.get('/file', [
+  query('search').isLength({max: 40}).optional(),
+  query('page').isInt({min: 1}).optional(),
+  query('limit').isInt({min: 5, max: 100}).optional(),
+  query('sort').isIn(['date', 'size']).optional(),
+  query('nonkoleksi').isBoolean().optional(),
+  checkValidation
+], (req, res) => {
+  const search = req.query.search || ''
+  const limit = req.query.limit || 20
+  const page = req.query.page || 1
+  let fileQuery = File.find({
+    name: {
+      '$regex': search
+    },
+  })
+  .skip((page - 1) * limit)
+  .limit(limit)
+  .sort('-date')
   .populate({
     path: 'uploader',
     select: '_id username'
@@ -17,20 +37,20 @@ router.get('/file', (req, res) => {
     path: 'koleksi.data',
     select: '_id title year'
   })
-  .then(files => {
-    res.json({
+  if (req.query.nonkoleksi) {
+    fileQuery = fileQuery.where({
+      koleksi: null
+    })
+  }
+
+  return fileQuery.then(files => {
+    return res.json({
       success: true,
       message: 'Files found.',
       data: { files }
     })
   })
-  .catch(err => {
-    res.json({
-      success: false,
-      message: 'Database error.',
-      error: err
-    })
-  })
+  .catch(catchErr)
 })
 
 module.exports = router
